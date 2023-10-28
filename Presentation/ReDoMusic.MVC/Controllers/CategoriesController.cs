05fb0d89e7a056409cfdcefdfe2d1f0b271a9de0
tarikemir <81103446+tarikemir@users.noreply.github.com>
2023-10-28 23:47:38 +0300

    DeleteCategory Action Has Written

---
+++ Presentation/ReDoMusic.MVC/Controllers/CategoriesController.cs
@@ -0,0 +1,30 @@
+﻿using DoReMusic.Domain.Entities;
+using DoReMusic.Persistence.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoReMusic.MVC.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public readonly DoReMusicDbContext _context;
+        public CategoriesController()
+        {
+            _context = new DoReMusicDbContext();
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+        public IActionResult DeleteCategory(string id)
+        {
+            Category category = _context.Categories.Where(x => x.Id == Guid.Parse(id)).FirstOrDefault();
+            
+            _context.Categories.Remove(category);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
